cdd0c4024924ef4a470df73a902423b659fbb689
rbd3 <rabedesanaa@gmail.com>
2023-06-07 10:35:31 +0300

    Fix linters error

--- modules/tasks.js
+++ modules/tasks.js
@@ -1,23 +1,23 @@
 export default class TODoTasks {
   constructor () {
     this.tasks = [
-       {
+      {
         id: 0,
         description: 'Wash the dishes',
         completed: false,
-       },
-       {
+      },
+      {
         id: 1,
         description: 'complete To Do list project',
         completed: false,
-       },
-       {
+      },
+      {
         id: 2,
         description: 'To do today',
         completed: false,
-       },
-     ];
-   }
+      },
+    ];
+  }
 
   add(task) {
     task.completed = false;
